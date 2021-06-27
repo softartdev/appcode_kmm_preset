edfdbd4879a3445585c4906ce9195a4519172da8
artur <artik222012@gmail.com>
2021-06-27 09:39:28 +0500

    update gradle deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 buildscript {
     repositories {
         gradlePluginPortal()
-        jcenter()
         google()
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20-RC-184")
-        classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+        classpath("com.android.tools.build:gradle:4.2.1")
     }
 }
 
